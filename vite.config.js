a50f08f05c08c42f96cf9c49d7dd391a5c7fb7ea
SauravPatel64 <sauravpatel220930@gmail.com>
2025-12-15 08:34:04 +0530

    seond

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "CODE-Creator",
+  base: "/CODE-Creator/",
   plugins: [react(), tailwindcss()],
   compilerOptions: {
     baseUrl: ".",
